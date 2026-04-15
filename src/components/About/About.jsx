d8b8d2abba2c064526f3ef97dd03fa0175aa4ca2
Dada Temiloluwa <dadatemiloluwa2000@gmail.com>
2026-04-15 23:51:57 +0100

    Updated the About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <section>
       <h2>About This Project</h2>
-      <p>This is a single-page React app with multiple components.</p>
+      <p>This is a single-page React website by Dada Temiloluwa with multiple components.</p>
     </section>
   );
 }
